dbbd0dcbe8c084715f0f3ab47372b6329f0f857f
imadeddineF <imadeddinefillalipro@gmail.com>
2023-12-16 21:37:10 +0100

    GTK testing

--- main.c
+++ main.c
@@ -1,41 +1,93 @@
 #include <gtk/gtk.h>
 
-static void activate(GtkApplication *app, gpointer user_data)
+typedef struct
 {
-  GtkWidget *window;
-  GtkWidget *label;
+  GtkWidget *stack;
+  GtkTextBuffer *buffer;
+} StackVisualizer;
 
-  // Create a new window
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Hello GTK");
-  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-
-  // Create a new label with the text "Hello, World!"
-  label = gtk_label_new("Hello World!");
-
-  // Add the label to the window
-  gtk_container_add(GTK_CONTAINER(window), label);
-
-  // Show all widgets in the window
-  gtk_widget_show_all(window);
+void insert_element(GtkWidget *widget, gpointer user_data)
+{
+  StackVisualizer *visualizer = (StackVisualizer *)user_data;
+  // Implement stack insert operation
+  // Update visualization
 }
 
-int main(int argc, char **argv)
+void delete_element(GtkWidget *widget, gpointer user_data)
 {
-  GtkApplication *app;
-  int status;
+  StackVisualizer *visualizer = (StackVisualizer *)user_data;
+  // Implement stack delete operation
+  // Update visualization
+}
 
-  // Create a new GTK application
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+void create_element(GtkWidget *widget, gpointer user_data)
+{
+  StackVisualizer *visualizer = (StackVisualizer *)user_data;
+  // Implement stack create operation
+  // Update visualization
+}
 
-  // Connect the "activate" signal to the activate function
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+void search_element(GtkWidget *widget, gpointer user_data)
+{
+  StackVisualizer *visualizer = (StackVisualizer *)user_data;
+  // Implement stack search operation
+  // Update visualization
+}
 
-  // Run the application
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+void draw_stack(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  // Set a white background
+  cairo_set_source_rgb(cr, 1.0, 1.0, 1.0); // RGB values for white
+  cairo_paint(cr);
 
-  // Release resources
-  g_object_unref(app);
+  // Implement drawing logic based on the stack
+  // You can add additional drawing code here
+}
 
-  return status;
+int main(int argc, char *argv[])
+{
+  gtk_init(&argc, &argv);
+
+  StackVisualizer visualizer;
+
+  // Create the main window
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Stack Visualizer");
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  // Create a vertical box to hold widgets
+  GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  // Create a drawing area
+  GtkWidget *drawing_area = gtk_drawing_area_new();
+  gtk_widget_set_size_request(drawing_area, 400, 300);
+  g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_stack), &visualizer);
+  gtk_box_pack_start(GTK_BOX(vbox), drawing_area, TRUE, TRUE, 0);
+
+  // Create buttons for stack operations
+  GtkWidget *insert_button = gtk_button_new_with_label("Insert");
+  GtkWidget *delete_button = gtk_button_new_with_label("Delete");
+  GtkWidget *create_button = gtk_button_new_with_label("Create");
+  GtkWidget *search_button = gtk_button_new_with_label("Search");
+
+  // Connect button signals to the callback function
+  g_signal_connect(insert_button, "clicked", G_CALLBACK(insert_element), &visualizer);
+  g_signal_connect(delete_button, "clicked", G_CALLBACK(delete_element), &visualizer);
+  g_signal_connect(create_button, "clicked", G_CALLBACK(create_element), &visualizer);
+  g_signal_connect(search_button, "clicked", G_CALLBACK(search_element), &visualizer);
+
+  // Add buttons to the vertical box
+  gtk_box_pack_start(GTK_BOX(vbox), insert_button, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), delete_button, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), create_button, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), search_button, FALSE, FALSE, 0);
+
+  // Show all widgets
+  gtk_widget_show_all(window);
+
+  // Run the GTK main loop
+  gtk_main();
+
+  return 0;
 }
